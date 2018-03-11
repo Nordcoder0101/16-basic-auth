a2d920ec214b1d1a950d00db0819e28bbb6e87bd
Greg <greg@Gregs-MacBook-Air.local>
2018-03-11 11:56:50 -0700

    linter errors fixed and add eslintrc file

--- lab-greg/__test__/auth-route.test.js
+++ lab-greg/__test__/auth-route.test.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const request = require('superagent');
-const mongoose = require('mongoose');
-const Promise = require('bluebird');
 const User = require('../model/user.js');
 const serverToggle = require('../lib/server-toggle.js');
 const server = require('../server.js');
@@ -14,8 +12,8 @@ const url = 'http://localhost:3000';
 const exampleUser = {
   username: 'exampleuser',
   password: '1234',
-  email: 'exampleuser@test.com'
-}
+  email: 'exampleuser@test.com',
+};
 
 describe('Auth Routes', function() {
   beforeAll( done => {
@@ -36,19 +34,19 @@ describe('Auth Routes', function() {
     describe('with a valid body', function() {
       afterEach( done => {
         User.remove({})
-        .then( () => done())
-        .catch(done);
+          .then( () => done())
+          .catch(done);
       });
 
       it('should return a token', done => {
         request.post(`${url}/api/signup`)
-        .send(exampleUser)
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.status).toEqual(200);
-          expect(typeof res.text).toEqual('string');
-          done();
-        });
+          .send(exampleUser)
+          .end((err, res) => {
+            if (err) return done(err);
+            expect(res.status).toEqual(200);
+            expect(typeof res.text).toEqual('string');
+            done();
+          });
       });
     });
   });
@@ -64,29 +62,29 @@ describe('Auth Routes', function() {
       beforeEach( done => {
         let user = new User(exampleUser);
         user.generatePasswordHash(exampleUser.password)
-        .then( user => user.save())
-        .then( user => {
-          this.tempUser = user;
-          done();
-        })
-        .catch(done);
+          .then( user => user.save())
+          .then( user => {
+            this.tempUser = user;
+            done();
+          })
+          .catch(done);
       });
 
       afterEach( done => {
         User.remove({})
-        .then( () => done())
-        .catch(done)
+          .then( () => done())
+          .catch(done);
       });
 
       it('should return a token', done => {
         request.get(`${url}/api/signin`)
-        .auth('exampleuser', '1234')
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.status).toEqual(200);
-          expect(typeof res.text).toEqual('string');
-          done();
-        });
+          .auth('exampleuser', '1234')
+          .end((err, res) => {
+            if (err) return done(err);
+            expect(res.status).toEqual(200);
+            expect(typeof res.text).toEqual('string');
+            done();
+          });
       });
     });
   });
